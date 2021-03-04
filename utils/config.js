fc1338c8d862a725cc4cb9133e7aebc83d653f55
GonzaloHer97 <gonza.herher@gmail.com>
2021-03-04 22:46:57 +0100

    GonzaloHer97

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 "use strict";
 module.exports = {
         mysqlConfig: {
-                host: "209.97.189.133",      // Ordenador que ejecuta el SGBD
-                user: "fdihelperuser",      // Usuario que accede a la BD
-                password: "fdihelper",     // Contraseña con la que se accede a la BD
+                host: "localhost",      // Ordenador que ejecuta el SGBD
+                user: "root",      // Usuario que accede a la BD
+                password: "",     // Contraseña con la que se accede a la BD
                 database: "fdihelper"     // Nombre de la base de datos
          },
-        port: 3000,                     // Puerto en el que escucha el servidor
+        port: 3600,                     // Puerto en el que escucha el servidor
 }
